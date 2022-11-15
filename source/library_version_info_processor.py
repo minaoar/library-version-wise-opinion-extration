058a7f259b333028d92ec00b732f60812fb6ead3
minaoar <minaoar@yahoo.com>
2022-11-15 14:19:56 -0700

    Get latest three release versions prior the post date

--- source/library_version_info_processor.py
+++ source/library_version_info_processor.py
@@ -44,11 +44,44 @@ def process_version_info(library_name):
     version_info = version_info[['version', 'usage', 'days', 'date_min', 'date_max']]
 
     print(version_info)
+    return version_info
 
+def adjust_cool_down_period(date):
+    date = pd.to_datetime(date)
+
+    # add cool down period to the date
+    # currently this is set to 0. We can change this to 30 days or 60 days if we think that 
+    # a post in StackOverflow should only come after a release passes this designated cool down period.
+    cool_down_period = 30 
+    date = date - pd.Timedelta(days=cool_down_period)
+    # print("Date after cool down period: " + str(date))
+
+    return date
+
+def get_tentative_versions(library, date):
+    # adjust cool down period
+    # date = adjust_cool_down_period(date)
+
+    # convert the date to datetime
+    date = pd.to_datetime(date)
+    version_info = process_version_info(library)
+
+    # get the latest three versions that were released before the date
+    tentative_versions = version_info[version_info['date_min'] < date].tail(3)
+
+    # sort the versions by usage
+    tentative_versions = tentative_versions.sort_values(by=['usage'], ascending=False)
+
+    # keep only the version text and convert the series to list
+    tentative_versions = tentative_versions['version'].tolist()
+    fancy_print(tentative_versions)
+    return tentative_versions
 
 
 if __name__ == "__main__":
-    process_version_info("gson")
-    process_version_info("jackson")
-    process_version_info("spring-orm")
-    process_version_info("hibernate")
+    # process_version_info("gson")
+    # process_version_info("jackson")
+    # process_version_info("spring-orm")
+    # process_version_info("hibernate")
+
+    get_tentative_versions("jackson", '2022-01-01')
